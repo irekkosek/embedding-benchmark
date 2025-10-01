1d54aecdf22846d632dea2e9abd7350f281eb596
MRosiak <maciejrosiak@protonmail.com>
2025-10-01 18:05:33 +0200

    x

--- main.go
+++ main.go
@@ -55,6 +55,7 @@ func main() {
 					start := time.Now()
 					_, err := emb.GenerateEmbeddings(ctx, []embedding.EmbeddingPayload{{Data: text, ID: int64(j.id)}})
 					if err == nil {
+						fmt.Printf("GenerateEmbeddings error: %v\n", err)
 						processed.Add(1)
 						tookMs := float64(time.Since(start).Microseconds()) / 1000.0
 						durationsMu.Lock()
